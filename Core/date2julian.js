798694ca51bf331c5d6427602464a81d0e81a7c8
Edgar <edgargc.upc@gmail.com>
2021-07-31 18:45:13 +0200

    Installing linternts checkers and documentation generators

--- Core/date2julian.js
+++ Core/date2julian.js
@@ -2,49 +2,38 @@
  * Astrodynamics | Kepler Solver
  * Author: Edgar Gago Carrillo
  * Date 16/03/2021
- * 
+ *
  * Inputs:
  * Calendar date format: year, month, day, hour, minute, second
- 
  * Outputs:
  * JD: Julian Day
- * 
+ *
  * Description:
  * Function that generates the Julian date from a traditional date time format
  * more info about Juñian dates:
- * 
- * 
+ *
+ *
  */
 
-
 exports.date2julian = (date) => {
+  if (date === undefined) {
+    return 0
+  } else if (Object.keys(date).length < 6) {
+    return Error('Falta uno o más parámetros en el objeto date')
+  }
 
+  // eslint-disable-next-line array-callback-return
+  Object.keys(date).map((param) => {
+    if (date[param] === undefined) return 0
+  })
 
-    if (date === undefined) {
-        return 0
-    } else if (Object.keys(date).length < 6) {
-        return Error("Falta uno o más parámetros en el objeto date")
-    }
+  const a = Math.floor((14 - date.month) / 12)
+  const y = date.year + 4800 - a
+  const m = date.month + 12 * a - 3
 
-    Object.keys(date).map(param => {
-        if (date[param] === undefined) return 0;
+  // Compute Julian Day Number
+  const JDN = date.day + Math.floor((153 * m + 2) / 5) + 365 * y + Math.floor(y / 4) - 32083
 
-
-    })
-
-    const a = Math.floor((14 - date.month) / 12);
-    const y = date.year + 4800 - a;
-    const m = date.month + 12 * a - 3;
-
-
-    // Compute Julian Day Number
-    const JDN = date.day + Math.floor((153 * m + 2) / 5) + 365 * y + Math.floor(y / 4) - 32083;
-
-    // Find Julian Date
-    const JD = JDN + (date.hour - 12) / 24 + date.minute / 1440 + date.second / 86400;
-
-    return JD;
+  // Find Julian Date
+  return JDN + (date.hour - 12) / 24 + date.minute / 1440 + date.second / 86400
 }
-
-
-
